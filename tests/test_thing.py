fb63b475bc3bf3568bc4081dcb2045951fae254c
Raphael Gaschignard <raphael@rtpg.co>
2026-02-23 21:45:54 +1000

    HACK: reset calls

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 from biz.views import aget_client_count_orm
 import pytest
+import pytest_asyncio
 from biz.models import Client
 
 from django_async_backend.db import async_connections
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def truncate_biz_tables(django_db_setup):
+    """Ensure biz tables are clean before each test in this module."""
+    # XXX I feel like we shouldn't need to do this??
+    async with async_connections["default"].cursor() as cursor:
+        await cursor.execute(
+            "TRUNCATE TABLE biz_invoice, biz_client RESTART IDENTITY CASCADE"
+        )
 
 
 def test_thing():
